95f7bec3256c14e2eb729cb6cf185a0faf466189
Leen Said <leensaid24@gmail.com>
2026-05-08 10:18:31 +0300

    refactor: ZED-Qt

--- ZED-Qt/include/frame_data.h
+++ ZED-Qt/include/frame_data.h
@@ -4,24 +4,6 @@
 #include <QVector>
 #include <QMetaType>
 
-namespace protocol {
-
-constexpr uint32_t MAGIC = 0x54524156;
-constexpr int HEADER_SIZE = 24;
-
-#pragma pack(push, 1)
-struct Header {
-    uint32_t magic;
-    uint32_t seq;
-    uint64_t timestamp_ns;
-    uint32_t nr;
-    uint32_t nt;
-};
-#pragma pack(pop)
-
-static_assert(sizeof(Header) == 24, "Header must be exactly 24 bytes");
-
-} // namespace protocol
 
 struct FrameData {
     uint32_t seq; // probably not useful while using commmanager
@@ -35,3 +17,4 @@ struct FrameData {
 };
 
 Q_DECLARE_METATYPE(FrameData)
+
